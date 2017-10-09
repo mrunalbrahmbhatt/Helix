4ed60f7dafa02218d68f389d4f9fa7ca8ef924c1
Maria Fel <maria@partechit.nl>
2017-10-09 12:47:23 +0200

    Build-publish-local: 1. Configuration is created 2. Solution path param is deleted 3. comments are added

--- configuration/scripts/Build/build-publish-local.ps1
+++ configuration/scripts/Build/build-publish-local.ps1
@@ -1,5 +1,54 @@
 <#
     .SYNOPSIS
+    This script contains the Add-Feature and Add-Foundation methods which can be used to add a new module to a Sitecore Helix based Visual Studio solution.
+    
+    The Visual Studio solution should contain a add-helix-module-configuration.json file containing variables which this script will use.
+    
+    The Add-Feature and Add-Foundation methods can be run from the Pacakge Console Manager as long as this script is loaded in the relevant PowerShell profile. 
+    Run $profile in the Pacakge Manager Console to verify the which profile is used.
+#>
+
+# Some hardcoded values
+$buildPublishConfigFile = "build-publish-configuration.json"   # Used in Add-Module.
+
+<#
+    .SYNOPSIS
+    Creates a config object which is used in the other functions in this script file.
+
+    .DESCRIPTION
+    This function should be considered private and is called from the Add-Module function.
+
+    .Parameter JsonConfigFilePath
+    The path of the json based configuration file which contains the path to the module-template folder,
+    namespaces and tokens to replace.   
+
+#>
+function Create-Config
+{
+    Param(
+        [Parameter(Position=0, Mandatory=$True)]
+        [string]$JsonConfigFilePath       
+    )
+
+    $jsonFile = Get-Content -Raw -Path "$JsonConfigFilePath" | ConvertFrom-Json
+    
+    if ($jsonFile)
+    {
+        $config = New-Object psobject
+        Add-Member -InputObject $config -Name SlnName -Value $jsonFile.config.slnName -MemberType NoteProperty
+        Add-Member -InputObject $config -Name DestinationDirectoryPath -Value $jsonFile.config.destinationDir -MemberType NoteProperty
+        Add-Member -InputObject $config -Name SegmentMarker -Value $jsonFile.config.segmentMarker -MemberType NoteProperty
+		
+        [System.Collections.ArrayList]$arrList=@()
+		$arrList +=@($jsonFile.config.filesToExclude)      
+		
+        Add-Member -InputObject $config -Name FilesToExclude  -Value $arrList -MemberType NoteProperty 
+        
+        return $config
+    }
+}
+<#
+    .SYNOPSIS
     The main function that parses $ProjectFile and returns Array of paths to [ContentItems + all inside of bin folder].
 
     .DESCRIPTION
@@ -30,7 +79,6 @@ function Extract-Content
     }
 
 	$bin = Join-Path  $absolutePathToParent "bin"
-	#$files += @($bin)
 	$binFiles = Get-ChildItem $bin -Recurse | ForEach-Object{		 
 		 if (Test-Path $_.FullName -pathType leaf)
 		 {			
@@ -49,6 +97,8 @@ function Extract-Content
 
     .PARAMETER ArrayToBeFiltered
     Array that has to be filtered.
+
+	.PARAMETER FilterArray
 
 #>
 function ArrayFilter () 
@@ -69,9 +119,14 @@ function ArrayFilter ()
     .DESCRIPTION
     This function should be considered private and is called from the function.
 
-    .PARAMETER ArrayToBeFiltered
+    .PARAMETER SlnDir
     Array that has to be filtered.
 
+	 .PARAMETER SlnName
+    Array that has to be filtered.
+
+	 .PARAMETER FilesToExclude
+    Array that has to be filtered.
 #>
 function Get-ProjectsPaths
 {
@@ -84,9 +139,7 @@ function Get-ProjectsPaths
         [string[]]$FilesToExclude
      )
 
-	  $slnFilePath = Join-Path $SlnDir $SlnName
-
-	  Write-Host "Get All Content Items from Projects of  $($slnFilePath) except of $($FilesToExclude)" -foregroundcolor green  	
+	  $slnFilePath = Join-Path $SlnDir $SlnName		
 
 	  $slnfiles = @();
 
@@ -104,107 +157,277 @@ function Get-ProjectsPaths
 
 	 $csprojs = $filteredArray | ?{ $_ -match ".csproj$" }			
 
+	 
+
 	 $projItems= @();
 	 $csprojs| 
 		Foreach {
 			$filePath = Join-Path $SlnDir $_
 
-			Write-Host "file path  $($filePath)" -foregroundcolor green 				
+			Write-Host "$($filePath)" 				
 			$projItems += @($filePath)
 		}		 
 		  
 	return $projItems;
 }
+<#
+    .SYNOPSIS
+    The main function that is filtering out FilterArray from ArrayToBeFiltered.
 
-Function Get-AllFilesPathsToPublish($sourceSlnDir, $slnName, $filesToExclude)
+    .DESCRIPTION
+    This function should be considered private and is called from the function.
+
+    .PARAMETER ArrayToBeFiltered
+    Array that has to be filtered.
+#>
+function Get-AllFilesPathsToPublish
 {
-	$projItems = Get-ProjectsPaths $sourceSlnDir $slnName $filesToExclude
+	 Param(
+        [Parameter(Position=0, Mandatory=$True)]
+        [string]$SlnDir,
+	    [Parameter(Position=1, Mandatory=$True)]
+        [string]$SlnName,
+		[Parameter(Position=2, Mandatory=$True)]
+        [string[]]$FilesToExclude
+     )
+
+    $projItems = Get-ProjectsPaths $SlnDir $SlnName $FilesToExclude
 
 	$contentItems= @();
 	$projItems| Foreach {
 	              $contentItems += @(extract-content $_)
 			  }
-		 
+	
+	Write-Host "Files that will be published: " 
 	$contentItems | Foreach {
-				 Write-Host "$($_)" -foregroundcolor green 
+				 Write-Host "$($_)" -foregroundcolor green  
 				}
 
 	return $contentItems;  
 }
+<#
+    .SYNOPSIS
+    The main function that is filtering out FilterArray from ArrayToBeFiltered.
 
-Function Ensure-Dir($destinationFolder)
+    .DESCRIPTION
+    This function should be considered private and is called from the function.
+
+    .PARAMETER DestinationFolder
+    Array that has to be filtered.
+#>
+function Ensure-Dir()
 {
-	if (!(Test-Path $destinationFolder -PathType Container)) {
-                      New-Item -ItemType Directory -Force -Path $destinationFolder
+	 Param(
+        [Parameter(Position=0, Mandatory=$True)]
+        [string]$DestinationFolder
+	    
+     )
+
+	if (!(Test-Path $DestinationFolder -PathType Container)) {
+                      New-Item -ItemType Directory -Force -Path $DestinationFolder
                   } 
 }
+<#
+    .SYNOPSIS
+    The main function that is filtering out FilterArray from ArrayToBeFiltered.
 
-Function Copy-FilesToDestination($destinationDir, $sourceFiles, $segmentMarker)
+    .DESCRIPTION
+    This function should be considered private and is called from the function.
+
+    .PARAMETER DestinationDir
+    Array that has to be filtered.
+
+	 .PARAMETER SourceFiles
+    Array that has to be filtered.
+
+	 .PARAMETER SegmentMarker
+    Array that has to be filtered.
+#>
+function Copy-FilesToDestination
 {
-	 $sourceFiles| Foreach {
+	Param(
+        [Parameter(Position=0, Mandatory=$True)]
+        [string]$DestinationDir,
+		[Parameter(Position=1, Mandatory=$True)]
+        [string[]]$SourceFiles,
+		[Parameter(Position=2, Mandatory=$True)]
+        [string]$SegmentMarker	    
+     )
+
+	 $SourceFiles| Foreach {
 		          $sourceFolder = Split-Path $_	 
 		         
-		          $pathSegments = $sourceFolder -Split $segmentMarker 	          
+		          $pathSegments = $sourceFolder -Split $SegmentMarker 	          
 		         
-		          $destinationFolder = Join-Path $destinationDir $pathSegments[1]
+		          $destinationFolder = Join-Path $DestinationDir $pathSegments[1]
 		          
 		          Ensure-Dir $destinationFolder
 
 		          Copy-Item $_ $destinationFolder -Recurse 
 			  }   
 }
+<#
+    .SYNOPSIS
+    The main function that is filtering out FilterArray from ArrayToBeFiltered.
 
-Function Delete-DirIfExists($dirPath){
-	if (Test-Path $dirPath ) {
-	Remove-Item $dirPath -Recurse -Force
-		}
-}
+    .DESCRIPTION
+    This function should be considered private and is called from the function.
 
-
-Function Get-TempDirPath($destinationDir)
+    .PARAMETER DirPath
+    Array that has to be filtered.	
+#>
+function Delete-DirIfExists
 {
-	$tempDestinationPath = Split-Path $destinationDir
+	Param(
+        [Parameter(Position=0, Mandatory=$True)]
+        [string]$DirPath	
+       
+     )
+
+	if (Test-Path $DirPath ) 
+	{
+	  Remove-Item $DirPath -Recurse -Force
+    }
+}
+<#
+    .SYNOPSIS
+    The main function that is filtering out FilterArray from ArrayToBeFiltered.
+
+    .DESCRIPTION
+    This function should be considered private and is called from the function.
+
+    .PARAMETER DestinationDir
+    Array that has to be filtered.	
+#>
+function Get-TempDirPath
+{
+	Param(
+        [Parameter(Position=0, Mandatory=$True)]
+        [string]$DestinationDir	       
+     )
+
+	$tempDestinationPath = Split-Path $DestinationDir
 
 	$tempDestinationPath = Join-Path $tempDestinationPath "Temp"		
 
 	return $tempDestinationPath
 }
+<#
+    .SYNOPSIS
+    The main function that is filtering out FilterArray from ArrayToBeFiltered.
 
-Function Publish-AllToDir($sourceSlnDir, $slnName,  $destinationDir, $segmentMarker, $filesToExclude)
+    .DESCRIPTION
+    This function should be considered private and is called from the function.
+
+    .PARAMETER SourceSlnDir
+    Array that has to be filtered.	
+	.PARAMETER SlnName
+    Array that has to be filtered.	
+	.PARAMETER DestinationDir
+    Array that has to be filtered.	
+	.PARAMETER SegmentMarker
+    Array that has to be filtered.	
+	.PARAMETER FilesToExclude
+    Array that has to be filtered.	
+#>
+function Publish-AllToDir
 {
-	$tempDestinationPath = Get-TempDirPath $destinationDir	
+	Param
+	(
+        [Parameter(Position=0, Mandatory=$True)]
+        [string]$SourceSlnDir,
+		[Parameter(Position=1, Mandatory=$True)]
+        [string]$SlnName,	
+		[Parameter(Position=2, Mandatory=$True)]
+        [string]$DestinationDir,	
+		[Parameter(Position=3, Mandatory=$True)]
+        [string]$SegmentMarker,	
+		[Parameter(Position=4, Mandatory=$True)]
+        [string[]]$FilesToExclude		
+     )
+
+	$tempDestinationPath = Get-TempDirPath $DestinationDir	
+
+	Write-Output "Temporary directory for publishing is created $($tempDestinationPath)." 
+
+	$filesToPublish = Get-AllFilesPathsToPublish $SourceSlnDir $SlnName	$FilesToExclude
+
+	Copy-FilesToDestination $tempDestinationPath $filesToPublish $SegmentMarker	
+
+	Write-Output "Copying files from $($tempDestinationPath) to $($DestinationDir)." 
+	
+	Copy-Item $tempDestinationPath $DestinationDir -Recurse 
+
+	Write-Output "All files are successfully copied." 
 
 	Delete-DirIfExists $tempDestinationPath
 
-	$filesToPublish = Get-AllFilesPathsToPublish $sourceSlnDir $slnName	$filesToExclude
-
-	Copy-FilesToDestination $tempDestinationPath $filesToPublish $segmentMarker	
-
-	Copy-Item $tempDestinationPath $destinationDir -Recurse -Force
-
-	Delete-DirIfExists $tempDestinationPath
+	Write-Output "Temporary directory $($tempDestinationPath) is removed." 
 }
 
-Import-Module -Name "C:\Program Files (x86)\WindowsPowerShell\Modules\Invoke-MsBuild\2.6.0\Invoke-MsBuild.psm1"
-Function Build-Publish-Local($sourceSlnDir, $slnName,  $destinationDir, $segmentMarker, $filesToExclude)
+function Run
 {
-	$slnPath = Join-Path $sourceSlnDir $slnName
-	Write-Host "Executing MSBuild for $($slnPath)..."
-	$build = Invoke-MsBuild -Path $slnPath -MsBuildParameters "/target:Build" 
+   # try
+  #  {
+        # Do a check if there is a solution active in Visual Studio.
+        # If there is no active solution the Add-Projects function would fail.
 
-	if ($build.BuildSucceeded -eq $true)
-    {
-        Write-Output "Build completed successfully!"
-    }
-    else
-    {
-        Write-Output "Build failed!"
-        Write-Host (Get-Content -Path $build.BuildLogFilePath)
+		Write-Output "Solution is found. Solution name is $($dte.Solution.FullName)"
 
-        Exit 1
-    }
+		$solutionFullPath = $dte.Solution.FullName
+        if (-not $solutionFullPath)
+        {
+            throw [System.ArgumentException] "There is no active solution."
+        }
 
-	Publish-AllToDir $sourceSlnDir $slnName $destinationDir $segmentMarker $filesToExclude
+        # The only reason I do this check is because I need a path to start searching for the json based config file. 
+        $solutionRootFolder = [System.IO.Path]::GetDirectoryName($solutionFullPath)
+
+		Write-Output "Solution folder is $($solutionRootFolder)"
+
+        if (-not (Test-Path "$solutionRootFolder"))
+        {
+            throw [System.IO.DirectoryNotFoundException] "$solutionRootFolder folder not found."
+        }
+
+        $configJsonFile = Get-ChildItem -Path "$solutionRootFolder" -File -Filter "$buildPublishConfigFile" -Recurse | Select-Object -First 1 | Select-Object -ExpandProperty FullName
+        if (-not (Test-Path $configJsonFile))
+        {
+            throw [System.IO.DirectoryNotFoundException] "$configJsonFile not found."
+        }
+
+        # Create a config object we can use throughout the other functions.
+        $config = Create-Config -JsonConfigFilePath "$configJsonFile" 
+
+		Write-Output "Script configuration is found. $($config)"		
+	   
+		Write-Output  "Executing MSBuild for $($solutionFullPath)..."
+
+	    $build = Invoke-MsBuild -Path $solutionFullPath -MsBuildParameters "/target:Build" 
+
+	    if ($build.BuildSucceeded -eq $true)
+        {
+          Write-Output "Build completed successfully!"
+        }
+        else
+        {
+          Write-Output "Build failed!"
+          Write-Host (Get-Content -Path $build.BuildLogFilePath)
+
+          Exit 1
+        }
+
+	    Write-Output "Publishing is started...."
+		Publish-AllToDir $solutionRootFolder $config.SlnName $config.DestinationDirectoryPath $config.SegmentMarker $config.FilesToExclude
+
+		Write-Output "Publishing to $($config.DestinationDirectoryPath) is successfully finished."
+	#}
+	#catch
+	#{
+	#	Write-Output "Exception... "
+	#	Write-Error $error[0]
+ #       exit
+	#}
 }
 
-Build-Publish-Local -sourceSlnDir D:\Projects\Internal\Labs\Helix -slnName Helix.sln -destinationDir D:\TestDeploy -segmentMarker code -filesToExclude (".Test.csproj", "NamespacePrefix.ModuleType.ModuleName.csproj", "NamespacePrefix.ModuleType.ModuleName.Tests.csproj", "Scripts.pssproj")
+Run
